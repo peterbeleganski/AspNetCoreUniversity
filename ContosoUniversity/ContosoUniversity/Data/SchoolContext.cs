f8f9bec67fb103cbe29766fbdb48b6d711f2331c
Petar Beleganski <petar.beleganski@scalefocus.com>
2018-12-01 18:53:19 +0200

    implemented ContosUniversity tutorial with data access

---
+++ ContosoUniversity/ContosoUniversity/Data/SchoolContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.Pages.Models;
+
+namespace ContosoUniversity.Models
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext (DbContextOptions<SchoolContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Student { get; set; }
+
+        public DbSet<Enrollment> Enrollment { get; set; }
+
+        public DbSet<Course> Course { get; set; }
+    }
+}
