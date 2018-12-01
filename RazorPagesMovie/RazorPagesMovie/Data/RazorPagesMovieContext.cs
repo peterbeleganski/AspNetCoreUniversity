fca23921f9814a5348d807b5eb12adb7d7af3800
Petar Beleganski <petar.beleganski@scalefocus.com>
2018-12-01 17:03:36 +0200

    implemented Razor Pages tutorial

---
+++ RazorPagesMovie/RazorPagesMovie/Data/RazorPagesMovieContext.cs
@@ -0,0 +1,16 @@
+﻿namespace RazorPagesMovie.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using RazorPagesMovie.Models;
+
+    public class RazorPagesMovieContext : DbContext
+    {
+        public RazorPagesMovieContext(DbContextOptions<RazorPagesMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
+
